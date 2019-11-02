277233d2ee8bae1ee803cbc1fa74d01cc130712c
giraffeman <giraffeman180@gmail.com>
2019-11-03 03:07:06 +0700

    first commit

---
+++ pages/a.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class APage extends React.Component {
+    render() {
+        return(
+            <h1>
+                A Page
+            </h1>
+        );
+    }
+}
+
+export default APage;
